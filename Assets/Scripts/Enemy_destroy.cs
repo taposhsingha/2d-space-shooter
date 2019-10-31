bfdcb19fcc15c376876f70f4d0ede94e563a0e54
taposhsingha <57071916+taposhsingha@users.noreply.github.com>
2019-10-31 16:10:57 +0600

    added explosion

--- Assets/Scripts/Enemy_destroy.cs
+++ Assets/Scripts/Enemy_destroy.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Enemy_destroy : MonoBehaviour
 {
+   public GameObject explosion;
+
    void OnTriggerEnter(Collider other)
    {
        if(other.tag=="Boundary")
        {
            return;
        }
+       Instantiate(explosion,transform.position,transform.rotation);
        Destroy(other.gameObject);
        Destroy(gameObject);
    }
